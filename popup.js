ec25424a2f66c6d2a2cbcea4cd4cc10fa42ddf4e
vasetodachev <123009036+vasetodachev@users.noreply.github.com>
2023-11-02 16:54:52 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,48 +1,72 @@
-const names = ["Vasil", "Lilly", "Atanas", "Stefan", "Panayot", "Nikol"];
-const nameList = document.getElementById("nameList-CC-Helper");
-const resetButton = document.getElementById("resetButton-CC-Helper");
+const teams = {
+  team1: ["Vasil", "Lilly", "Atanas", "Stefan", "Panayot", "Nikol"],
+  team2: ["Nikol", "Christina", "Delia", "Nayden", "Alina", "Stanislav"],
+};
 
-// Initialize name counters
-const counters = {};
+let currentTeam = "team1"; // Initialize with Team 1
 
-// Retrieve stored counter values, or initialize to 0 if not found
-chrome.storage.local.get(names, (result) => {
-  names.forEach((name) => {
-    counters[name] = result[name] || 0;
+// Load the last selected team from storage
+chrome.storage.local.get("currentTeam", (result) => {
+  if (result.currentTeam && (result.currentTeam === "team1" || result.currentTeam === "team2")) {
+    currentTeam = result.currentTeam;
+  }
+
+  // Continue with the rest of the code
+  const nameList = document.getElementById("nameList-CC-Helper");
+  const resetButton = document.getElementById("resetButton-CC-Helper");
+  const toggleTeamButton = document.getElementById("toggleTeamButton-CC-Helper");
+
+  // Initialize name counters
+  const counters = {};
+
+  // Retrieve stored counter values, or initialize to 0 if not found
+  chrome.storage.local.get([...teams.team1, ...teams.team2], (result) => {
+    [...teams.team1, ...teams.team2].forEach((name) => {
+      counters[name] = result[name] || 0;
+    });
+
+    // Update the counters displayed
+    updateCounters();
   });
 
   // Update the counters displayed
-  updateCounters();
-});
-
-// Update the counters displayed
-function updateCounters() {
-  nameList.innerHTML = "";
-  names.forEach((name) => {
-    const button = document.createElement("button");
-    button.textContent = `${name} (${counters[name]})`;
-    button.addEventListener("click", () => {
-      counters[name]++;
-      updateCounters();
-      saveCounters(); // Save the updated counter values
+  function updateCounters() {
+    nameList.innerHTML = "";
+    teams[currentTeam].forEach((name) => {
+      const button = document.createElement("button");
+      button.textContent = `${name} (${counters[name]})`;
+      button.addEventListener("click", () => {
+        counters[name]++;
+        updateCounters();
+        saveCounters();
+      });
+      nameList.appendChild(button);
     });
-    nameList.appendChild(button);
-  });
-}
+  }
 
-// Save the counter values in storage
-function saveCounters() {
-  chrome.storage.local.set(counters);
-}
+  // Save the counter values in Browser
+  function saveCounters() {
+    chrome.storage.local.set(counters);
+  }
 
-// Reset all counters
-resetButton.addEventListener("click", () => {
-  names.forEach((name) => {
-    counters[name] = 0;
+  // Reset all counters
+  resetButton.addEventListener("click", () => {
+    [...teams.team1, ...teams.team2].forEach((name) => {
+      counters[name] = 0;
+    });
+    updateCounters();
+    saveCounters();
   });
+
+  // Toggle between teams
+  toggleTeamButton.addEventListener("click", () => {
+    currentTeam = currentTeam === "team1" ? "team2" : "team1";
+    updateCounters();
+
+    // Save the current team to Browser
+    chrome.storage.local.set({ currentTeam });
+  });
+
+  // Initial update
   updateCounters();
-  saveCounters();
 });
-
-// Initial update
-updateCounters();
